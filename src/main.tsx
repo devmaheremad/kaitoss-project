705b95c4fc4900a77185e46bb4331ed67e8e1b35
Maher E. Elmashharawi <mashmaher96@gmail.com>
2023-01-24 13:23:52 +0200

    make fonts responsive

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,23 @@
+import {
+  createTheme,
+  responsiveFontSizes,
+  ThemeProvider,
+} from "@mui/material/styles";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
+let theme = createTheme();
+theme = responsiveFontSizes(theme);
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <App />
+      </Router>
+    </ThemeProvider>
   </React.StrictMode>
 );
